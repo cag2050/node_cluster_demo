2711afcce059b5d50e3696722a4db9e70863a7c0
cag2050 <cag2050@163.com>
2018-01-10 15:09:16 +0800

    m

--- cluster.js
+++ cluster.js
@@ -2,6 +2,8 @@ let os = require('os')
 let cluster = require('cluster')
 let http = require('http')
 
+let data = {key: 'value', hello: 'world'};
+
 if (cluster.isMaster) {
     let len = os.cpus().length
     for (let i = 0; i < len; i++) {
@@ -9,7 +11,7 @@ if (cluster.isMaster) {
     }
 } else {
     http.createServer((req, res) => {
-        res.writeHead(200)
-        res.end("hello node's cluster!")
+        res.writeHead(200, {'Content-Type': 'application/json'})
+        res.end(JSON.stringify(data))
     }).listen(11111)
 }
